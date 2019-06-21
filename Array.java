690ef8e7bbd42c39d53f89dde87f0122476b9537
kakisoft <kakisttab@gmail.com>
2019-06-22 04:03:39 +0900

    e

--- Array.java
+++ Array.java
@@ -1,4 +1,10 @@
+import java.util.*;
+
 public class Array {
+
+  // 配列のサイズは変更できません。したがって、技術的には配列から要素を削除することはできません。
+  // Arrays.asList は、プリミティブ型の配列をリスト化してくれない。
+  // List.add(int 追加位置, Object 追加する要素)
 
   public static void main(String[] args) {
     //============================
@@ -40,6 +46,46 @@ public class Array {
       System.out.println(sale);
     }
 
+    //============================
+    //        値の削除
+    //============================
+    // int[] ari1 = {1, 2, 3, 4, 5};
+    int[] ari1 = {3, 8, 9, 7, 6};
+    int loopCnt = 3;
+
+    List<Integer> intList = new ArrayList<Integer>();
+    for (int i : ari1)
+    {
+        intList.add(i);
+    }
+
+    for(int i=0; i<loopCnt; i++)
+    {
+      int tmpi = intList.get(intList.size() - 1);
+      intList.remove(intList.size() - 1);
+      intList.add(0, tmpi);
+    }
+
+    // for (int i : intList)
+    // {
+    //     System.out.println(i);
+    // }
+
+    
+    System.out.println("============");
+
+    int[] ret = new int[intList.size()];
+    int cnt = intList.size();
+    for(int i=0; i<cnt; i++)
+    {
+      ret[i] = intList.get(0);
+      intList.remove(0);
+    }    
+
+    for (int i : ret)
+    {
+        System.out.println(i);
+    }
 
   }
 
